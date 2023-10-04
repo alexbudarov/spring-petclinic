70a20f49e61724acc7abea513072477ebde4fa5d
budarov <budarov@haulmont.com>
2023-10-04 15:45:34 +0400

    add tips for solving "fetching just saved entity" problem

--- src/main/java/org/springframework/samples/petclinic/rest/ReceiptRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/ReceiptRestController.java
@@ -1,9 +1,12 @@
 package org.springframework.samples.petclinic.rest;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.owner.Receipt;
 import org.springframework.samples.petclinic.owner.ReceiptRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +17,8 @@ import java.util.Optional;
 public class ReceiptRestController {
 
     private final ReceiptRepository receiptRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public ReceiptRestController(ReceiptRepository receiptRepository) {
         this.receiptRepository = receiptRepository;
@@ -34,4 +39,18 @@ public class ReceiptRestController {
     public Receipt save(@RequestBody @Valid Receipt entity) {
         return receiptRepository.save(entity);
     }
+
+    // 1) does not work
+    // Receipt receipt = receiptRepository.save(entity);
+    // receipt = receiptRepository.findById(receipt.getId()).orElseThrow();
+
+    // 2) does not work
+    // Receipt receipt = receiptRepository.saveAndFlush(entity);
+    // receipt = receiptRepository.findById(receipt.getId()).orElseThrow();
+
+    // 3) works
+    // @Transactional
+    // entityManager.refresh(receipt);
+    // see also https://stackoverflow.com/questions/45491551/refresh-and-fetch-an-entity-after-save-jpa-spring-data-hibernate
+
 }
