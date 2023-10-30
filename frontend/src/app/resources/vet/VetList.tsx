50b0c240e3c68ce0d57d71e5b87f446b66391f68
budarov <budarov@haulmont.com>
2023-10-30 17:28:16 +0400

    rework filter in VetList into multiselect (ReferenceArrayInput)

--- frontend/src/app/resources/vet/VetList.tsx
+++ frontend/src/app/resources/vet/VetList.tsx
@@ -1,6 +1,5 @@
-import { Card, Typography } from "@mui/material";
-import { useState } from "react";
-import { AutocompleteInput, ChipField, Datagrid, FunctionField, List, ListContextProvider, ReferenceInput, ReferenceManyField, SingleFieldList, SortPayload, TextField, Title, useDataProvider } from "react-admin"
+import { Card } from "@mui/material";
+import { ChipField, Datagrid, ReferenceArrayInput, ReferenceManyField, SingleFieldList, SortPayload, TextField, Title, useDataProvider } from "react-admin"
 import { useQuery } from 'react-query';
 import { useForm, FormProvider } from 'react-hook-form';
 import { CustomDataProvider } from "../../../dataProvider";
@@ -9,13 +8,13 @@ import { CustomDataProvider } from "../../../dataProvider";
 export const VetList = () => {
     const dataProvider = useDataProvider<CustomDataProvider>();
     const filterForm = useForm();
-    const specialtyId = filterForm.watch("specialtyId");
+    const specialtyIds = filterForm.watch("specialtyIds");
 
     const { data: vets, isLoading } = useQuery(
-        ['specialty', 'vetsBySpecialties', { ids: specialtyId ? [specialtyId]: [] }], 
+        ['specialty', 'vetsBySpecialties', { ids: specialtyIds}], 
         () => {
-          if (specialtyId) {
-            return dataProvider.vetsBySpecialties(specialtyId ? [specialtyId]: [])
+          if (specialtyIds && specialtyIds.length > 0) {
+            return dataProvider.vetsBySpecialties(specialtyIds)
           } else {
             const params = {filter: {}, pagination: {page: 1, perPage: 100}, sort: {field: 'id', order: 'ASC'} as SortPayload};
             return dataProvider.getList('vet', params).then(a => a.data);
@@ -27,11 +26,9 @@ export const VetList = () => {
       <div>
         <Title title="Veterinarian list" />
         <FormProvider {...filterForm}>
-          <ReferenceInput label="Specialty" source="specialtyId" 
-              reference="specialty"
-          >
-            <AutocompleteInput />
-          </ReferenceInput>
+          <ReferenceArrayInput 
+            source="specialtyIds" 
+            reference="specialty"/>
         </FormProvider>
         <Card>
             <Datagrid 
