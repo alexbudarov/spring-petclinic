6be0e79f1c92a889b6054547eb26324e83215a99
budarov <budarov@haulmont.com>
2023-11-03 19:38:08 +0400

    Rework Specialty getList() filtering to use Specification.

--- src/main/java/org/springframework/samples/petclinic/rest/SpecialtyRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/SpecialtyRestController.java
@@ -1,14 +1,23 @@
 package org.springframework.samples.petclinic.rest;
 
+import jakarta.persistence.criteria.From;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
-import org.springframework.samples.petclinic.rest.rasupport.*;
+import org.springframework.samples.petclinic.rest.rasupport.RaFilter;
+import org.springframework.samples.petclinic.rest.rasupport.RaProtocolUtil;
+import org.springframework.samples.petclinic.rest.rasupport.RaRange;
+import org.springframework.samples.petclinic.rest.rasupport.RaSort;
 import org.springframework.samples.petclinic.vet.Specialty;
 import org.springframework.samples.petclinic.vet.SpecialtyRepository;
+import org.springframework.samples.petclinic.vet.Vet;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,25 +35,49 @@ public class SpecialtyRestController {
 
     @GetMapping
     public ResponseEntity<List<Specialty>> findAll(@RaFilter SpecialtyListFilter filter, RaRange range, RaSort sort) {
-        if (filter.id() != null) {
+
+        /*if (filter.id() != null) {
             Page<Specialty> page = specialtyRepository.findByIdIn(filter.id(), range.toPageable(sort));
             return raProtocolUtil.convertToResponseEntity(page, "specialty");
         }
-
-        // getManyReference()
         if (filter.vetId() != null) {
             Page<Specialty> page = specialtyRepository.findByVetId(filter.vetId(), range.toPageable(sort));
             return raProtocolUtil.convertToResponseEntity(page, "specialty");
-        }
+        }*/
 
-        Page<Specialty> page = specialtyRepository.findAll(range.toPageable(sort));
+        Specification<Specialty> specification = convertToSpecification(filter);
+        Page<Specialty> page = specialtyRepository.findAll(specification, range.toPageable(sort));
         var response = raProtocolUtil.convertToResponseEntity(page, "specialty");
         return response;
     }
 
+    private Specification<Specialty> convertToSpecification(SpecialtyListFilter filter) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            // getMany()
+            if (filter.id() != null) {
+                predicates.add(root.get("id").in((Object[]) filter.id()));
+            }
+
+            // getManyReference() in VetList
+            // custom condition
+            if (filter.vetId() != null) {
+                From<Vet, Vet> vetFrom = query.from(Vet.class);
+                Join<Vet, Specialty> specJoin = vetFrom.join("specialties");
+
+                predicates.add(criteriaBuilder.equal(vetFrom.get("id"), filter.vetId()));
+                predicates.add(criteriaBuilder.equal(specJoin, root));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
     public record SpecialtyListFilter(
-            Integer[] id, // used by DataProvider.getMany()
-            Integer vetId) {
+            Integer[] id,
+            Integer vetId
+    ) {
     }
 }
 
--- src/main/java/org/springframework/samples/petclinic/vet/SpecialtyRepository.java
+++ src/main/java/org/springframework/samples/petclinic/vet/SpecialtyRepository.java
@@ -3,13 +3,15 @@ package org.springframework.samples.petclinic.vet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface SpecialtyRepository extends JpaRepository<Specialty, Integer> {
+public interface SpecialtyRepository extends JpaRepository<Specialty, Integer>,
+        JpaSpecificationExecutor<Specialty> {
 
-    Page<Specialty> findByIdIn(Integer[] ids, Pageable pageable);
+    /*Page<Specialty> findByIdIn(Integer[] ids, Pageable pageable);
 
     @Query("select s from Vet v join v.specialties s where v.id = :vetId")
-    Page<Specialty> findByVetId(Integer vetId, Pageable pageable);
+    Page<Specialty> findByVetId(Integer vetId, Pageable pageable);*/
 
 }
