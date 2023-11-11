df5209736b07b81652b56468d3faef3d1b48abb8
budarov <albudarov@gmail.com>
2023-11-11 23:33:23 +0400

    make Pet#birthDate optional field, as it's not required in model and DB

--- frontend/src/app/resources/pet/PetCreate.tsx
+++ frontend/src/app/resources/pet/PetCreate.tsx
@@ -9,7 +9,7 @@ export const PetCreate = () => {
           <ReferenceField source="ownerId" reference="owner"/>
         </Labeled>
         <TextInput source="name" required autoFocus/>
-        <DateInput source="birthDate" required/>
+        <DateInput source="birthDate"/>
         <ReferenceInput source="typeId" reference="pet-type">
             <AutocompleteInput validate={required()} />
         </ReferenceInput>
