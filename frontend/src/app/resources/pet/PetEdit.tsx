42626f3481792f554485ad73aacb09f4e36230c5
budarov <budarov@haulmont.com>
2023-11-24 15:19:15 +0400

    customization: display list of related visits in PetEdit

--- frontend/src/app/resources/pet/PetEdit.tsx
+++ frontend/src/app/resources/pet/PetEdit.tsx
@@ -1,5 +1,6 @@
-import { AutocompleteInput, DateInput, Edit, Labeled, ReferenceField, ReferenceInput, SimpleForm, TextInput, required } from "react-admin";
+import { AutocompleteInput, Datagrid, DateField, DateInput, Edit, Identifier, Labeled, ReferenceField, ReferenceInput, SimpleForm, SortPayload, TextField, TextInput, required, useGetManyReference, useRecordContext } from "react-admin";
 import { redirectFromPetToOwner } from "./redirectFromPetToOwner";
+import { Typography } from "@mui/material";
 
 export const PetEdit = () => (
   <Edit redirect={redirectFromPetToOwner} mutationMode="pessimistic">
@@ -11,7 +12,38 @@ export const PetEdit = () => (
       <DateInput source="birthDate"/>
       <ReferenceInput source="typeId" reference="pet-type">
             <AutocompleteInput validate={required()} />
-        </ReferenceInput>
+      </ReferenceInput>
+      <PetVisits/>
     </SimpleForm>
   </Edit>
 )
+
+const PetVisits = () => {
+  const pet = useRecordContext();
+  const petId = pet?.id;
+
+  const sort: SortPayload = { field: 'date', order: 'DESC' };
+  const { data, total, isLoading } = useGetManyReference(
+      'visit',
+      { 
+        target: 'petId',
+        id: petId,
+        sort: sort
+      }
+  );
+  const visits = data || [];
+
+  return <>
+    <Typography variant="h6">Visits</Typography>
+    <Datagrid
+            data={visits}
+            total={total}
+            isLoading={isLoading}
+            sort={sort}
+            bulkActionButtons={false}
+        >
+      <DateField source="date" options={{dateStyle: 'long'}} />
+      <TextField source="description" />
+    </Datagrid>
+  </>
+}
