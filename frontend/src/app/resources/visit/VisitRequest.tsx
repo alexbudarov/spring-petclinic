bb9b893c2c3e84b9395b2f4aaabec292dec2d3f9
budarov <budarov@haulmont.com>
2023-12-04 18:25:12 +0400

    Step 9.2: add header & Owner drop-down. Search & show telephone.

--- frontend/src/app/resources/visit/VisitRequest.tsx
+++ frontend/src/app/resources/visit/VisitRequest.tsx
@@ -1,8 +1,25 @@
-import { Title } from "react-admin"
+import { AutocompleteInput, ReferenceInput, SimpleForm, TextInput, Title, required } from "react-admin"
+import { Typography } from "@mui/material"
 
 export const VisitRequest = () => {
     return <>
-        <Title title="Request Visit" />
-        <div>Request visit</div>
+      <Title title="Request Visit" />
+      <SimpleForm onSubmit={() => {}} maxWidth="30em">
+        <Typography variant="h6">
+          Enter visit details
+        </Typography>
+        <OwnerDropdown />
+      </SimpleForm>
     </>
 };
+
+const OwnerDropdown = () => {
+    const ownerFilterToQuery = (searchText: string) => ({ telephone: searchText });
+    const ownerOptionRenderer = (owner: any) => `${owner.firstName} ${owner.lastName} (${owner.telephone})`;
+
+    return (
+        <ReferenceInput source="ownerId" reference="owner">
+          <AutocompleteInput filterToQuery={ownerFilterToQuery} optionText={ownerOptionRenderer} fullWidth={true} validate={required()}/>
+        </ReferenceInput>
+    )
+}
