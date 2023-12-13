f55f402d6a3b8fb99901e6a8f69f2c15e56fa441
budarov <budarov@haulmont.com>
2023-12-13 16:42:16 +0400

    Step 4.3: Changes over react-admin defaults: app title,  dataProvider, proxy settings, limit auto-detected resource pages, auto-detected recordRepresentation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,26 +6,26 @@ import {
   ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { ownerRecordRepresentation } from "./app/functions/ownerRecordRepresentation";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource
       name="pet-type"
       list={ListGuesser}
-      edit={EditGuesser}
-      show={ShowGuesser}
+      recordRepresentation="name"
     />
     <Resource
       name="owner"
       list={ListGuesser}
-      edit={EditGuesser}
-      show={ShowGuesser}
+      recordRepresentation={ownerRecordRepresentation}
     />
     <Resource
       name="pet"
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
+      recordRepresentation="name"
     />
   </Admin>
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+    import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -9,6 +9,11 @@ export default defineConfig({
     },
     server: {
         host: true,
+        proxy: {
+            '/rest': {
+                target: 'http://localhost:8080'
+            }
+        }
     },
     base: './',
 });
