7cea7f793f67f76fb34e9a63a3fc656712988760
budarov <budarov@haulmont.com>
2023-12-13 20:28:21 +0400

    Step 9.15: register custom endpoint requestVisit() in data provider

--- frontend/src/dataProvider.ts
+++ frontend/src/dataProvider.ts
@@ -11,7 +11,21 @@ const httpClient = fetchUtils.fetchJson;
 
 export interface CustomDataProvider extends DataProvider {
   checkAvailability: (specialtyId: number, date: Dayjs) => Promise<boolean>;
+  requestVisit: (request: NewVisitRequest) => Promise<RequestVisitResponse>;
     // custom endpoints 
+}
+
+type NewVisitRequest = {
+  petId: number;
+  specialtyId: number;
+  date: string;
+  description: string;
+}
+
+type RequestVisitResponse = {
+  success: boolean;
+  visitId: number | null;
+  errorMessage: string | null;
 }
 
 export const dataProvider: CustomDataProvider = {
@@ -20,6 +34,14 @@ export const dataProvider: CustomDataProvider = {
     checkAvailability: function (specialtyId: number, date: Dayjs): Promise<boolean> {
       return httpClient(`/rest/visit/check-availability?specialtyId=${specialtyId}&date=${formatDate(date)}`)
         .then(({ json }) => (json));
+    },
+
+    requestVisit: function (request: NewVisitRequest): Promise<RequestVisitResponse> {
+      return httpClient(`/rest/visit/request`, {
+          method: "POST",
+          body: JSON.stringify(request)
+      })
+      .then(({ json }) => (json));
     }
 
     // custom endpoints
