aeddde3cc91712a28b8a10213fc90df909a12cd7
budarov <budarov@haulmont.com>
2023-12-13 17:24:27 +0400

    Step 6.1: Add filter by owner to PetList

--- frontend/src/app/resources/pet/PetList.tsx
+++ frontend/src/app/resources/pet/PetList.tsx
@@ -1,8 +1,12 @@
-import { Datagrid, DateField, DeleteButton, EditButton, List, NumberField, ReferenceField, TextField } from "react-admin"
+import { Datagrid, DateField, DeleteButton, EditButton, List, NumberField, ReferenceField, ReferenceInput, TextField } from "react-admin"
+
+const filters = [
+  <ReferenceInput label="Owner" source="ownerId" reference="owner" alwaysOn={true} />
+]
 
 export const PetList = () => {
   return (
-    <List>
+    <List filters={filters}>
       <Datagrid bulkActionButtons={false}>
         <TextField source="name" sortable={false} />
         <DateField source="birthDate" sortable={false} />
